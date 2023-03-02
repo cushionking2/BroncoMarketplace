ac2ea6f96a07577e305fbe65100c93d482525f4e
cushionking2 <splash.wang@gmail.com>
2023-03-02 00:44:57 -0800

    added buy/sell filter slider

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ let searchForm = document.getElementById('search-form');
 let searchResultList = document.querySelectorAll('.result-item');
 let result = document.getElementById('search-results');
 let searchValue=result.getElementsByTagName("p");
+const checkbox = document.getElementById('color_mode')
+
 
 searchForm.addEventListener("keyup", function() {
     for (var i=0; i <searchValue.length; i++) {
@@ -28,7 +30,34 @@ searchForm.addEventListener("keyup", function() {
     }
 })
 
+checkbox.addEventListener('change', (event) => {
+    if (event.currentTarget.checked) {
+        var s_item = document.getElementsByClassName("selling-item");
+        for (var i = 0; i < s_item.length; i++) {
+           s_item[i].style.display = "none";
+        }
+
+        var b_item = document.getElementsByClassName("buying-item");
+        for (var i = 0; i < b_item.length; i++) {
+            b_item[i].style.display = "";
+         }
+    } else {
+        var s_item = document.getElementsByClassName("selling-item");
+        for (var i = 0; i < s_item.length; i++) {
+           s_item[i].style.display = "";
+        }
+
+        var b_item = document.getElementsByClassName("buying-item");
+        for (var i = 0; i < b_item.length; i++) {
+            b_item[i].style.display = "none";
+         }
+    }
+})
 
 
-
-
+function resetFilter() {
+    let res = document.getElementsByClassName('result-item');
+    for (var i = 0; i < res.length; i++) {
+        res[i].style.display = "";
+     }
+}
